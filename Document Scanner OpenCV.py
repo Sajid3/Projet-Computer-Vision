cdb6de4951e3c6f1057c9df8bfe8f629a2025fbb
Sajid Ali <mailsajidali5@gmail.com>
2019-08-22 17:39:30 +0530

    Add files via upload

---
+++ Document Scanner OpenCV.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+#import rect
+
+
+def rectify(h):
+    h = h.reshape((4,2))
+    hnew = np.zeros((4,2),dtype = np.float32)
+
+    add = h.sum(1)
+    hnew[0] = h[np.argmin(add)]
+    hnew[2] = h[np.argmax(add)]
+
+    diff = np.diff(h,axis = 1)
+    hnew[1] = h[np.argmin(diff)]
+    hnew[3] = h[np.argmax(diff)]
+
+    return hnew
+
+
+# Image .
+image = cv2.imread('test.jpg')
+
+# Resize image
+image = cv2.resize(image, (1500, 880))
+
+orig = image.copy()
+
+# convert to grayscale and blur to smooth
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+#blurred = cv2.medianBlur(gray, 5)
+
+# apply Canny Edge Detection
+edged = cv2.Canny(blurred, 0, 50)
+orig_edged = edged.copy()
+
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+(contours, _) = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+#x,y,w,h = cv2.boundingRect(contours[0])
+#cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),0)
+
+# get approximate contour
+for c in contours:
+    p = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * p, True)
+
+    if len(approx) == 4:
+        target = approx
+        break
+
+
+# mapping target points to 800x800 quadrilateral
+approx = rectify(target)
+pts2 = np.float32([[0,0],[800,0],[800,800],[0,800]])
+
+M = cv2.getPerspectiveTransform(approx,pts2)
+dst = cv2.warpPerspective(orig,M,(800,800))
+
+cv2.drawContours(image, [target], -1, (0, 255, 0), 2)
+dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+
+
+# using thresholding on warped image to get scanned effect (If Required)
+ret,th1 = cv2.threshold(dst,127,255,cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(dst,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(dst,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+ret2,th4 = cv2.threshold(dst,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+
+cv2.imshow("Original.jpg", orig)
+cv2.imshow("Original Edged.jpg", orig_edged)
+cv2.imshow("Outline.jpg", image)
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
